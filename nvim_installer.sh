e9c952d9f7732ac1fe433396ce419a4a00b2324b
tmorita-bsc <tatsuya.morita@bosco-tech.com>
2017-12-25 22:37:01 +0900

    add docker directory and nvim_installer.sh

---
+++ nvim_installer.sh
@@ -0,0 +1,25 @@
+cd ~/
+git clone https://github.com/tmorita-bsc/dotfiles.git
+
+yum update -y
+yum install -y epel-release
+yum install -y gcc make wget git tmux tree vim
+curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo
+yum install -y neovim
+yum -y install readline-devel zlib-devel bzip2-devel sqlite-devel openssl-devel
+yum -y groupinstall "Development Tools"
+
+git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+
+echo "export PYENV_ROOT=$HOME/.pyenv" >> ~/.bashrc
+echo "export PATH=$PYENV_ROOT/bin:$PATH" >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv install 3.6.3
+pyenv virtualenv 3.6.3 main
+pyenv global 3.6.3
+
+cd init_env.sh
+sudo sh init_env.sh
