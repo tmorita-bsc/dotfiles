0559d3d718bd56a5081e43e87bc5ba17af6423d0
tmorita-bsc <tatsuya.morita@bosco-tech.com>
2017-12-26 12:36:27 +0900

    modify

--- nvim_installer.sh
+++ nvim_installer.sh
@@ -9,17 +9,23 @@ yum install -y neovim
 yum -y install readline-devel zlib-devel bzip2-devel sqlite-devel openssl-devel
 yum -y groupinstall "Development Tools"
 
+export HOME=/root
+#export HOME=/home/user_name
 git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
 
-echo "export PYENV_ROOT=$HOME/.pyenv" >> ~/.bashrc
-echo "export PATH=$PYENV_ROOT/bin:$PATH" >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
+echo "export PYENV_ROOT=$HOME/.pyenv" >> $HOME/.bashrc
+echo "export PATH=$PYENV_ROOT/bin:$PATH" >> $HOME/.bashrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+source $HOME/.bashrc
 
 pyenv install 3.6.3
 pyenv virtualenv 3.6.3 main
 pyenv global 3.6.3
+
+echo 'export XDG_CONFIG_HOME = "$HOME/.config"' >> $HOME/.bash_profile
+echo 'export XDG_CACHE_HOME = "$HOME/.cache"' >> $HOME/.bash_profile
 
 cd init_env.sh
 sudo sh init_env.sh dein
@@ -32,3 +38,8 @@ sudo ./configure
 sudo make
 sudo make install
 ln -sf /usr/local/share/gtags/gtags.vim /usr/share/vim/vim74/plugin
+
+source $HOME/.bash_profile
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/nvim
+ln -sf $HOME/.vimrc $HOME/.config/nvim/init.vim
